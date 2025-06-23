692fc462ebc6af8c73e25ec1fd7d41946819a4fd
Daf <47498851+dafbeh@users.noreply.github.com>
2025-06-23 16:09:15 +0100

    Update vite.config.js
    Remove base for new domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss()
