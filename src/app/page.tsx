b7ed2e7d0e7f68a6ca2779d72baf9ff965fbf45b
dafbeh <47498851+dafbeh@users.noreply.github.com>
2026-02-24 19:52:03 +0000

    mobile swipe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,19 @@ import FloatingBar from '@/components/FloatingBar';
 import NavBar from '@/components/NavBar';
 import Home from '@/components/sections/Home';
 import Projects from '@/components/sections/Projects';
+import { useSwipeable } from 'react-swipeable';
 
 export default function HomePage() {
   const [activeSection, setActiveSection] = useState('home');
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setActiveSection('projects'),
+    onSwipedRight: () => setActiveSection('home')
+  });
+
   return (
     <div>
-      <main className="flex flex-col">
+      <main className="flex flex-col" {...handlers}>
         <NavBar activeSection={activeSection} setActiveSection={setActiveSection} />
         <FloatingBar />
 
